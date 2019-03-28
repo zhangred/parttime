ba3c55ecb67b560453285a5df9dcacbd883355c0
张少华 <zhangsh01@SH-zhangsh01.local>
2019-03-28 20:51:20 +0800

    thems

--- theme/assets/javascript/tools.js
+++ theme/assets/javascript/tools.js
@@ -808,71 +808,32 @@ dateTable.prototype = {
 
 function botmore(num,callback){
     this.num = num;
-    this.callback = callback;
-    this.bindevent();
     this.stop = true;
-};
-botmore.prototype = {
-    bindevent:function(){
-        var _this = this;
-        var body = document.body||document.documentElement,
-            win_h = body.offsetHeight;
 
-        this.bodyscroll = function(event){
-            var timer;
-            return function(){
-                clearTimeout(timer)
-                timer = setTimeout(function(){
-                    _this.callback();
-                },100)
+    var _this = this;
+    var body = document.body||document.documentElement,
+        win_h = body.offsetHeight,
+        timer = '';
+
+    this.bodyscroll = function(event){
+        clearTimeout(timer);
+        if(_this.stop){return false;};
+        timer = setTimeout(function(){
+            var scrolltop=document.documentElement.scrollTop||document.body.scrollTop,
+                bodyh = body.scrollHeight;
+
+            if(scrolltop+win_h+num>bodyh){
+               callback();
             }
-            // debounce(function(e){
-            //     _this.callback();
-            // },100)
-            // // _this.callback();
-            // clearTimeout(timer);
-            // // if(_this.stop){return false;};
-            // timer = setTimeout(function(){
-            // //     var scrolltop=document.documentElement.scrollTop||document.body.scrollTop,
-            // //         bodyh = body.offsetHeight;
-            // //     if(scrolltop+win_h+_this.num>bodyh){
-            //         _this.callback();
-            //     // }
-                
-            // },100);
-        };
-        this.bindscroll();
-    },
-    bindscroll:function(){
-        var _this = this;
-        window.addEventListener('scroll', _this.bodyscroll,false);
-    },
-    unbindscroll:function(){
-         var _this = this;
-         window.removeEventListener('scroll',_this.bodyscroll,false);
+            
+        },100);
+    };
+    window.addEventListener('scroll', _this.bodyscroll,false);
+
+    this.unbindscroll = function(){
+        window.removeEventListener('scroll',_this.bodyscroll,false);
     }
+
 };
 
-function debounce(fn, delay) {
-    console.log(4)
-    // 定时器，用来 setTimeout
-    var timer
-  
-    // 返回一个函数，这个函数会在一个时间区间结束后的 delay 毫秒时执行 fn 函数
-    return function () {
-  
-      // 保存函数调用时的上下文和参数，传递给 fn
-      var context = this
-      var args = arguments
-  
-      // 每次这个返回的函数被调用，就清除定时器，以保证不执行 fn
-      clearTimeout(timer)
-  
-      // 当返回的函数被最后一次调用后（也就是用户停止了某个连续的操作），
-      // 再过 delay 毫秒就执行 fn
-      timer = setTimeout(function () {
-        fn.apply(context, args)
-      }, delay)
-    }
-  }
 
