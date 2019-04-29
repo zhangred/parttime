670a8d3394576648911bca44202c94bdb4eb75b7
张少华 <zhangsh01@SH-zhangsh01.local>
2019-04-29 16:29:53 +0800

    jiewu

--- jiewu/pages/new.js
+++ jiewu/pages/new.js
@@ -3,62 +3,105 @@ var util = require('../utils/util.js');
 
 Page({
 	data: {
-		tabs:[],
-		actab:1,
-		list:[]
-
-	},
-	onShow:function(){
-		this.getlist();
+		data:{list:[]}
 	},
 	onLoad: function () {
 		wx.setNavigationBarTitle({
-			title: '阅读打卡'
+			title: '发帖'
 		});
-
-		this.gettabs();
-	},
-	// 获取标签
-	gettabs(){
-		var that = this;
-		wx.request({
-			url: 'http://phocc11vv.bkt.clouddn.com/callback.json',
-			method: "GET",
-			data: {},
-			success (res) {
-				if(res.data.code==0){
-					//模拟数据
-					res.data = {
-						"data":[
-							{"id":2,"paixu":1,"timeCreate":"2018-11-12T03:34:13","timeUpdate":"2018-11-12T03:34:14","title":"灏变笟淇濋殰"},
-							{"id":3,"paixu":2,"timeCreate":"2018-11-12T03:34:13","timeUpdate":"2018-11-12T03:34:14","title":"浼樿川鏁欒偛"}
-						],
-						"code":200,
-						"msg":"OK",
-						"success":true
-					};
-
-					res.data.data.map(function(item){
-						item.created_at = util.formatTime(new Date(item.timeCreate));
-						return item;
-					})
-					console.log(res.data.data)
-					that.setData({
-						tabs:res.data.data,
-						actab:res.data.data[0].id
-					});
-					that.getlist();
-				}
-			}
-		})
-	},
-	//切换标签
-	changetab(e){
-		this.setData({
-			actab:e.currentTarget.dataset.id
-		})
-		this.getlist();
-	},
+    },
+    //绑定文案
+    setdata(e){
+        var val = e.detail.value,
+            key = e.target.dataset.key,
+            obj = {};
+        obj['data.'+key] = val;
+        this.setData(obj);
+    },
+    //上传图片
+    addimg(){
+        var that = this;
+        wx.chooseImage({
+            count: 1,
+            success(res) {
+              const tempFilePaths = res.tempFilePaths[0];
+              wx.showLoading({
+                title: '正在上传中···',
+              })
+              setTimeout(function(){
+                wx.hideLoading();
+                var dt = that.data.data;
+                dt.list.push(tempFilePaths);
+                that.setData({data:dt})
+              },1000)
+            //   wx.uploadFile({
+            //     url: 'https://example.weixin.qq.com/upload', // 仅为示例，非真实的接口地址
+            //     filePath: tempFilePaths[0],
+            //     name: 'file',
+            //     formData: {
+            //       user: 'test'
+            //     },
+            //     success(res) {
+            //       const data = res.data
+            //       // do something
+            //     }
+            //   })
+            }
+          })
+    },
+    seeimg(e){
+        var url = e.target.dataset.url,
+            that = this;
+        wx.previewImage({
+            current: url,
+            urls: that.data.data.list
+        })
+    },
+    delimg(e){
+        var idx = e.target.dataset.idx,
+            that = this;
+        wx.showModal({
+            title: '提示',
+            content: '确认删除此图片？',
+            success(res) {
+                if (res.confirm) {
+                    var dt = that.data.data;
+                    dt.list.splice(idx,1);
+                    that.setData({data:dt});
+                }
+            }
+        })
+    },
+    senttopic(){
+        var data = this.data.data;
+        if(!data.title){
+            wx.showToast({
+                title: '请输入标题',
+                icon: 'none',
+                duration: 2000
+              })
+            return;
+        }
+        if(!data.content){
+            wx.showToast({
+                title: '请输入话题描述',
+                icon: 'none',
+                duration: 2000
+              })
+            return;
+        }
+        wx.showLoading({
+            title: '正在发送中···',
+        })
+        setTimeout(function(){
+            wx.hideLoading();
+            wx.showToast({
+                title: '提交成功',
+                icon: 'success',
+                duration: 2000
+            })
+        },1000)
+    },
 	//文章列表
 	getlist(){
 		var that = this;
